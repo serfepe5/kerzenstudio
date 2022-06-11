3834e6d84447d6f7e4df242502a34308f07b1e82
serfepe5 <nueva1357@gmail.com.com>
2022-06-11 16:15:32 +0200

    primer commit

---
+++ src/main/java/com/tfg/kerzenstudio/KerzenstudioApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.kerzenstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KerzenstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KerzenstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfg/kerzenstudio/KerzenstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.kerzenstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KerzenstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
